fba767dd6d8d2f8eeab60d792e5c829443612848
VicK <57548498+CyberBounty@users.noreply.github.com>
2020-05-07 16:17:54 +0700

    Update masternode.js

--- js/etc/wallet/masternode.js
+++ js/etc/wallet/masternode.js
@@ -10,8 +10,7 @@ var masternode = getURL(window.location.search.substring(1)).ref;
 
 if (masternode){
   localStorage.setItem("ref", masternode)
-  $(".dashboard-link").attr("href", "/use.html?ref=" + localStorage.getItem('ref'))
-} else {
+  } else {
   localStorage.setItem("ref", siteMasternode)
 }
 
