d22bede6f527c46dd7b578b2cf312e8e08f659ec
VicK <57548498+CyberBounty@users.noreply.github.com>
2020-05-08 13:05:05 +0700

    use.js dialog js

--- js/etc/wallet/use.js
+++ js/etc/wallet/use.js
@@ -160,3 +160,34 @@ $('#copyMNButton').on('click', function (){
 $(".home").click(function(){
     window.location.href = "/";
 });
+
+
+$("#barcode").replaceWith('<img src="https://chart.googleapis.com/chart?chs=200x200&amp;cht=qr&amp;chl=' + myCropAddress + '&amp;choe=UTF-8" class="modal" />');
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
