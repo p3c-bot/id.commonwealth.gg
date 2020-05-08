2c1776bf45bf892930173e0d6a2c11521e8f60e2
VicK <57548498+CyberBounty@users.noreply.github.com>
2020-05-08 20:31:12 +0700

    Update home.js

--- js/etc/home.js
+++ js/etc/home.js
@@ -6,3 +6,7 @@ var masternode = getURL(window.location.search.substring(1)).ref;
 if (masternode){
 	 localStorage.setItem("ref", masternode)
 }
+
+$(document).ready(function(){
+$(".preloader").fadeOut();
+})
