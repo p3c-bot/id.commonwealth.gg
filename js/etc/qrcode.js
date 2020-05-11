48f7b788c84f9ba0cfa58462f34aa60ec52f85b3
VicK <57548498+CyberBounty@users.noreply.github.com>
2020-05-11 07:49:32 +0700

    Update qrcode.js

--- js/etc/qrcode.js
+++ js/etc/qrcode.js
@@ -1,18 +1,1 @@
 $("#qrImage").replaceWith('<img src="https://chart.googleapis.com/chart?chs=180x180&amp;cht=qr&amp;chl=' + myCropAddress + '&amp;choe=UTF-8" />');
-
-
-function openQRCode(node) {
-var reader = new FileReader();
-reader.onload = function() {
-node.value = "";
-qrcode.callback = function(res) {
-if(res instanceof Error) {
-alert("No Crop QrCode found.");
-} else {
-node.parentNode.previousElementSibling.value = res;
-}
-};
-qrcode.decode(reader.result);
-};
-reader.readAsDataURL(node.files[0]);
-}
