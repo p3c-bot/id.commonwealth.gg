906fd5f310984b92c78a91511da9ed9d7f4e507a
VicK <57548498+CyberBounty@users.noreply.github.com>
2020-05-12 18:41:36 +0700

    Update lobby.js
    aded fix global

--- js/etc/tribes/lobby.js
+++ js/etc/tribes/lobby.js
@@ -1,88 +1,99 @@
-$.getJSON("https://api.p3c.io/tribes/list", function (json) {
-  console.log(json)
-  $('#tribes').DataTable({
-    responsive: true,
-    pageLength: 10,
-    order: [[3, 'desc']],
-    data: json,
-    columns: [
-      {
-        data: 'tribe',
-        render: function (data, type, row) {
-          return linkify(data, "/tribe.html?id=" + data + "#")
-        }
-      },
-      {
-        data: 'name',
-        render: function (data, type, row) {
-          return linkify(data, "/tribe.html?id=" + row.tribe + "#")
-        }
-      },
-      {
-        data: 'players',
-        render: function (data, type, row) {
-          if (data.includes(playerAddress)){
-            return "✔️"
-          } else {
-            return "🔘"
+
+$('.ui.dropdown')
+    .dropdown({
+        action: 'activate',
+        onChange: function(value, text, $selectedItem) {
+          $('#tribes').DataTable().clear().destroy();
+          if (value == '0'){
+            buildLobby('https://api.p3c.io/tribes/list')
+            $('#title').text('Tribes Aktif')
+          }
+    
+          if (value == '1'){
+            address = web3.eth.accounts[0]
+            buildLobby('https://api.p3c.io/tribes/member/' + address)
+            $('#numDays').text(value + ' Days')
+            $('#title').text('Tribes Saya')
+          }
+    
+          if (value == '2'){
+            buildLobby('https://api.p3c.io/tribes/completed')
+            $('#title').text('Tribes Selesai')
           }
         }
-      },
-      {
-        data: 'power',
+    });
+;
+
+buildLobby('https://api.p3c.io/tribes/list')
+
+function buildLobby(url){
+  $.getJSON(url, function (json) {
+    $('#tribes').DataTable({
+      responsive: true,
+      pageLength: 15,
+      order: [[3, 'desc']],
+      data: json,
+      columns: [
+        {
+          data: 'tribe',
+          render: function (data, type, row) {
+            return linkify(data, "/tribe.html?id=" + data + "#")
+          }
+        },
+        {
+          data: 'name',
+          render: function (data, type, row) {
+            return linkify(data, "/tribe.html?id=" + row.tribe + "#")
+          }
+        },
+        {
+          data: 'players',
+          render: function (data, type, row) {
+            if (data.includes(playerAddress)){
+              return "✔️"
+            } else {
+              return "🔘"
+            }
+          }
+        },
+        {
+          data: 'power',
+          render: function (data, type, row) {
+            return (row.cost * row.waiting * 1000).toFixed(0)
+          }
+        },
+        {
+        data: 'cost',
         render: function (data, type, row) {
-          return (row.cost * row.waiting * 1000).toFixed(0)
+          return data + ' ETC'
         }
-      },
-      {
-      data: 'cost',
-      render: function (data, type, row) {
-        return data + ' ETC'
-      }
-      },
-      {
-        data: 'size',
-        render: function (data, type, row) {
-          return row.waiting + " / " + data
+        },
+        {
+          data: 'size',
+          render: function (data, type, row) {
+            return row.waiting + " / " + data
+          }
+        },
+        {
+          data: 'age',
+          render: function (data, type, row) {
+            return timeSince(Number(data * 1000))
+          }
+        },
+        {
+          data: 'reward',
+          render: function (data, type, row) {
+            return data + ' Point'
+          }
+        },
+        {
+          data: 'creator',
+          render: function (data, type, row) {
+            return data.substring(0,8) + "...";
+          }
         }
-      },
-      {
-        data: 'age',
-        render: function (data, type, row) {
-          return timeSince(Number(data * 1000))
-        }
-      },
-      {
-        data: 'reward',
-        render: function (data, type, row) {
-          return data + ' Point'
-        }
-      },
-      {
-        data: 'creator',
-        render: function (data, type, row) {
-          return data.substring(0,8) + "...";
-        }
-      },
-  //     // {
-  //     //   data: 'contract',
-  //     //   render: function (data, type, row) {
-  //     //     if (row.contract == "" || digest[row.contract.toLowerCase()] == undefined) {
-  //     //       return "Unknown"
-  //     //     }
-  //     //     return linkify(row.contract.substring(0, 6)+ '...', "https://blockscout.com/etc/mainnet/address/" + data + "/transactions")
-  //     //   }
-  //     // },
-  //     // {
-  //     //   data: 'audit',
-  //     //   render: function (data, type, row) {
-  //     //     if (data != "") {
-  //     //       return linkify("Yes", data);
-  //     //     } else {
-  //     //       return "No"
-  //     //     }
-  //     //   }
-  //     // },
-    ]
+      ]
+    });
+    $('#tribes_length').hide()
   });
-});
+}
